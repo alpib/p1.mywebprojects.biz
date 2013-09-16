24eb40beabb9b2d0a3b377b5ffbadcfcde063889
alpib <alpanabarua@g.harvard.edu>
2013-09-16 18:30:03 -0400

    changed the content to add education

--- index.php
+++ index.php
@@ -15,12 +15,13 @@
         
         <h3>A little about me</h3>
         <p>
-    	Hello! I am a former Lotus Notes Developer with about 7 years of work experience.  I quit my job to be at home with my family several years ago. 
+    	Hello! I am a former Lotus Notes Developer with about 7 years of work experience.  I graduated from Boston University (a while back) with a major in Environmental Policy and minor in Computer Science. 
+    	I quit my job to be at home with my family several years ago. 
 Did lots of volunteer work at school and community. At this point  
 I would like to refresh/update and learn new skills, possibly go back to work.  I am contemplating doing a masters degree in IT.  Trying out a few courses to determine if this is the right path for me to continue in.  
 </p>
 <p>
-	I live in Bethesda, Maryland (in the Washington, DC metro area).  Some of the activities I enjoy are reading, hiking, biking, dancing, cooking, period movies, science fiction and crime shows.
+	I live in Bethesda, Maryland (in the Washington, DC metro area).  Some of the activities I enjoy are reading, hiking, biking, travelling and dancing.
 </p>
 
 
