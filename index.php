8f333186eae464142d7b928a3e1102acae1a13b6
Alpana Barua <alpanabarua@g.harvard.edu>
2013-09-16 21:21:35 -0400

    Added Code Editor and HTML validation links

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
     <link rel="stylesheet" href="styles/file.css">
   </head>
   <body>
-  	<div id="containing">
+  	<div id="containing"> <!-- The main container surroundig all the rest-->
+  		<!-- Header -->
   		<div id="mainheader">
         <h1>Alpana Barua</h1>
-        <div id="content">
+            <div id="content">
 
-        <h4>Project 1 for CSCI E-15</h4>
+        <h2>Project 1 for CSCI E-15</h2>
+        <!-- End Header -->
         
+        <!-- Beginning content-->
         <h3>A little about me</h3>
         <p>
     	Hello! I am a former Lotus Notes Developer with about 7 years of work experience.  I graduated from Boston University (a while back) with a major in Environmental Policy and minor in Computer Science. 
@@ -20,15 +23,22 @@
 Did lots of volunteer work at school and community. At this point  
 I would like to refresh/update and learn new skills, possibly go back to work.  I am contemplating doing a masters degree in IT.  Trying out a few courses to determine if this is the right path for me to continue in.  
 </p>
-<p>
-	I live in Bethesda, Maryland (in the Washington, DC metro area).  Some of the activities I enjoy are reading, hiking, biking, travelling and dancing.
+<p>I live in Bethesda, Maryland.  Some of the activities I enjoy are reading, hiking, biking, travelling and dancing.
 </p>
 
+<!-- Code editor and Validation W3C HTML 5-->
 
-</div>
+<ul>
+  <li><h4>The code editor I am using:</h4>
+  	<a href="http://www.sublimetext.com">Sublime Text</a></li>
+  <li><h4>HTML 5 Validation:</h4>
+  	<a href="http://validator.w3.org/check?uri=www.p1.mywebprojects.biz%2Findex.php&charset=%28detect+automatically%29&doctype=Inline&group=0">My Page Validates</a></li>
+</ul>
 
-</div>
-</div>
+<p>
 
-  </body>
-</html>
+            </div>
+        </div>
+    </div>
+ 
+  </body> </html>
